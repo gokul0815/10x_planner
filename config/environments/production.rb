03f2684b74fef8ddcdc901b969b00cf6733d77fd
simposk <simposk@gmail.com>
2016-09-07 16:41:59 +0300

    Change production.rb settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Save files from dev to production
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
